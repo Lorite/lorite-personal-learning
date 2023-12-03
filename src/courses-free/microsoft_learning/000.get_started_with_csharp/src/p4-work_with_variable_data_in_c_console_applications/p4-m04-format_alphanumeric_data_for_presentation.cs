081504005c44b201b6975de853442122ed8fc50c
Lorite <a.lorite.mora@gmail.com>
2023-12-03 17:13:45 +0000

    Complete Microsoft learning course | Get started with C# | Part 4 Fixes #16

--- src/courses-free/microsoft_learning/000.get_started_with_csharp/src/p4-work_with_variable_data_in_c_console_applications/p4-m04-format_alphanumeric_data_for_presentation.cs
+++ src/courses-free/microsoft_learning/000.get_started_with_csharp/src/p4-work_with_variable_data_in_c_console_applications/p4-m04-format_alphanumeric_data_for_presentation.cs
@@ -44,8 +44,37 @@ namespace Lorite.GetStartedWithCSharp.P4M04FormatAlphanumericDataForPresentation
             yourDiscount += $"A discount of {((price - salePrice)/price):P2}!";
             Console.WriteLine(yourDiscount);
 
-            // Exercise - Explore string interpolation
+            // Padding and alignment
+            string textToPad = "Pad this";
+            Console.WriteLine(textToPad.PadLeft(12)); // textToPad.PadLeft(12) will pad the string with spaces to the left so that the total length of the string is 12 characters. So it will add 4 spaces to the left of the string.
+            Console.WriteLine(textToPad.PadRight(12)); // textToPad.PadRight(12) will pad the string with spaces to the right so that the total length of the string is 12 characters. So it will add 4 spaces to the right of the string.
+            Console.WriteLine(textToPad.PadLeft(12, '-')); // PadLeft() and PadRight() can also take a second parameter that specifies the character to use for padding. This is an overloaded method. It will add 4 dashes to the left of the string.
             
+            // Exercise - Complete a challenge to apply string interpolation to a form letter
+            Console.WriteLine("Challenge: Write a form letter");
+            string customerName = "Ms. Barros";
+            string currentProduct = "Magic Yield";
+            int currentShares = 2975000;
+            decimal currentReturn = 0.1275m;
+            decimal currentProfit = 55000000.0m;
+            string newProduct = "Glorious Future";
+            decimal newReturn = 0.13125m;
+            decimal newProfit = 63000000.0m;
+            Console.WriteLine($"Dear {customerName},");
+            Console.WriteLine($"As a customer of our {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return.\n");
+            Console.WriteLine($"Currently, you own {currentShares:N} shares at a return of {currentReturn:P}.\n");
+            Console.WriteLine($"Our new product, {newProduct} offers a return of {newReturn:P}.  Given your current volume, your potential profit would be {newProfit:C}.\n");
+            Console.WriteLine("Here's a quick comparison:\n");
+
+            string comparisonMessage = "";
+            comparisonMessage = currentProduct.PadRight(20);
+            comparisonMessage += String.Format("{0:P}", currentReturn).PadRight(10);
+            comparisonMessage += String.Format("{0:C}", currentProfit).PadRight(20);
+            comparisonMessage += "\n";
+            comparisonMessage += newProduct.PadRight(20);
+            comparisonMessage += String.Format("{0:P}", newReturn).PadRight(10);
+            comparisonMessage += String.Format("{0:C}", newProfit).PadRight(20);
+            Console.WriteLine(comparisonMessage);
         }
 
     }
