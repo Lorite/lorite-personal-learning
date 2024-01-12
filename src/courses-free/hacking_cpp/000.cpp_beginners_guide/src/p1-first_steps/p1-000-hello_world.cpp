ec60201a6a61e8c20e202105b19ddee9dd7785b9
Lorite <a.lorite.mora@gmail.com>
2024-01-12 15:28:28 +0000

    Complete hackingcpp courses | C++ Beginner's Guide | Hello world Fixes #19

---
+++ src/courses-free/hacking_cpp/000.cpp_beginners_guide/src/p1-first_steps/p1-000-hello_world.cpp
@@ -0,0 +1,10 @@
+#include <iostream>  // iostream is a header file that defines the standard input/output stream objects
+
+// main is the function that is called when the program is run
+int main() {
+  std::cout
+      << "Hello World\n";  // std::cout is a stream object that represents the standard output stream
+}
+
+// Compile: bazel build src/courses-free/hacking_cpp/000.cpp_beginners_guide:p1-000-hello_world
+// Run: bazel-bin/src/courses-free/hacking_cpp/000.cpp_beginners_guide:p1-000-hello_world
